6a73d0b735de7d6d13eedab56ecdd4acd43a0749
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-24 16:43:57 +0500

    projects commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 
 // Components
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <div className="card">
         <Hero />
+        <Projects />
       </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,24 +7,30 @@ import "../styles/Hero.css";
 import "boxicons";
 
 function Hero() {
-  return (
-    <div>
-        <center>
-            <img src="/avatar.jpg" className='avatar' alt="image" />
-        </center>
-        <div className="name">
-            <p>Hi, I'm Artyom</p>
-            <box-icon type='solid' name='badge-check' color="dodgerblue" size="28px"></box-icon>
+    return (
+        <div className='Hero'>
+            <center>
+                <img src="/avatar.jpg" className='avatar' alt="image" />
+            </center>
+            <div className="name">
+                <p>Hi, I'm Artyom</p>
+                <box-icon type='solid' name='badge-check' color="dodgerblue" size="28px"></box-icon>
+            </div>
+            <center>
+                <p className="desc">Experienced web developer based in Ashgabat known for creating intuitive and engaging user experiences.</p>
+            </center>
+            <div className="links">
+                <a href="#">
+                    <box-icon type='solid' name='phone-call' size="20px" color="grey"></box-icon>
+                    <span>Book a Call</span>
+                </a>
+                <a href="mailto:artyom.agadzhanyan421@gmail.com">
+                    <box-icon name='envelope' type='solid' size="20px" color="grey"></box-icon>
+                    <span>Address</span>
+                </a>
+            </div>
         </div>
-        <center>
-            <p className="desc">Experienced web developer based in Ashgabat known for creating intuitive and engaging user experiences.</p>
-        </center>
-        <div className="links">
-            <a href="#">Book a Call</a>
-            <a href="#">Address</a>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
