9126f0cae33d96b866eeb6f9c7f7d1f005f810b9
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-24 09:51:56 +0500

    navbar commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// CSS
+import "../styles/Hero.css";
+
+function Hero() {
+  return (
+    <div className='Hero'>
+      <p className="subHead">freelance web developer</p>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// CSS
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className='Navbar'>
+        <nav>
+            <a href="#" className="logo" style={{ display: "flex", width: "fit-content" }}>
+                <img src="/logo.svg" alt="logo" />
+            </a>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Projects</a></li>
+              <li><a href="#">Workflow</a></li>
+              <li><a href="#">Pricing</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+            <a href="#" className='email'>artyom.agadzhanyan421@gmail.com</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
