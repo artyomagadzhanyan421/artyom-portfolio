a0bf874ce45ca3dfacb11189d3567c7adb3d53ff
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-24 11:27:15 +0500

    hero commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css';
 // Components
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Projects />
+      <div className='background-grid'></div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,12 @@ function Navbar() {
             </a>
             <ul>
               <li><a href="#">Home</a></li>
-              <li><a href="#">Projects</a></li>
+              <li><a href="#projects">Projects</a></li>
               <li><a href="#">Workflow</a></li>
               <li><a href="#">Pricing</a></li>
               <li><a href="#">Contact</a></li>
             </ul>
-            <a href="#" className='email'>artyom.agadzhanyan421@gmail.com</a>
+            <a href="mailto:artyom.agadzhanyan421@gmail.com" className='email'>artyom.agadzhanyan421@gmail.com</a>
         </nav>
     </div>
   )
