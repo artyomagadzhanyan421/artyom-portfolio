4d2892b17e9a52a08a27ff22c0ca494724fe49a4
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-24 12:02:33 +0500

    delete commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import './App.css';
 
-// Components
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
-      <Projects />
-      <div className='background-grid'></div>
+
     </div>
   )
 }
