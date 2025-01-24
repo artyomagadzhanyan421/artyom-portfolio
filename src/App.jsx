0eb37e53a22266dc84f45ba5b4fff10e320428cd
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-24 09:23:51 +0500

    delete commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import './App.css';
 
-// Components
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Services from './components/Services';
-import Socials from './components/Socials';
-
 function App() {
   return (
     <div className="App">
-      <div className="card">
-        <Hero />
-        <Projects />
-        <Services />
-        <Socials />
-      </div>
+      
     </div>
   )
 }
