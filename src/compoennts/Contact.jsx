d36f13ac019aeaa795abbbe4cc27eb400e434c36
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-01-27 10:10:53 +0500

    arrange commit

--- src/compoennts/Contact.jsx
+++ src/compoennts/Contact.jsx
@@ -14,12 +14,7 @@ function Contact() {
         e.preventDefault();
 
         emailjs
-            .sendForm(
-                "service_8o03v5p",
-                "template_918opqp",
-                formRef.current,
-                "ZSiixRao-LhNSGWc8"
-            )
+            .sendForm("service_8o03v5p", "template_918opqp", formRef.current, "ZSiixRao-LhNSGWc8")
             .then(
                 (result) => {
                     alert("Message sent successfully!");
@@ -43,42 +38,18 @@ function Contact() {
                 <div>
                     <div className="name">
                         <p>Artyom Agadzhanyan</p>
-                        <box-icon
-                            type="solid"
-                            name="badge-check"
-                            size="22px"
-                            color="dodgerblue"
-                        ></box-icon>
+                        <box-icon type="solid" name="badge-check" size="22px" color="dodgerblue" />
                     </div>
                     <p className="job">Web-designer, developer</p>
                     <div className="socials">
-                        <a href="#" target="_blank" rel="noreferrer">
-                            <box-icon
-                                name="linkedin-square"
-                                type="logo"
-                                color="gray"
-                                size="19px"
-                            ></box-icon>
+                        <a href="#" target="_blank">
+                            <box-icon name="linkedin-square" type="logo" color="gray" size="19px" />
                         </a>
-                        <a
-                            href="https://github.com/artyomagadzhanyan421"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <box-icon
-                                name="github"
-                                type="logo"
-                                color="gray"
-                                size="19px"
-                            ></box-icon>
+                        <a href="https://github.com/artyomagadzhanyan421" target="_blank">
+                            <box-icon name="github" type="logo" color="gray" size="19px" />
                         </a>
-                        <a href="#" target="_blank" rel="noreferrer">
-                            <box-icon
-                                name="twitter"
-                                type="logo"
-                                color="gray"
-                                size="19px"
-                            ></box-icon>
+                        <a href="#" target="_blank">
+                            <box-icon name="twitter" type="logo" color="gray" size="19px" />
                         </a>
                     </div>
                 </div>
@@ -91,48 +62,19 @@ function Contact() {
                                 <p className="blockHeading">Contact</p>
                             </center>
                             <center>
-                                <p
-                                    className="blockDesc"
-                                    style={{ maxWidth: "350px" }}
-                                >
-                                    Fill out the form, to request for the project. I’ll respond
-                                    within 24 hours.
-                                </p>
+                                <p className="blockDesc" style={{ maxWidth: "350px" }}>Fill out the form, to request for the project. I’ll respond within 24 hours.</p>
                             </center>
                         </div>
                         <form ref={formRef} onSubmit={sendEmail}>
-                            <input
-                                type="email"
-                                name="user_email"
-                                placeholder="Email"
-                                required
-                            />
-                            <input
-                                type="text"
-                                name="subject"
-                                placeholder="Subject"
-                                required
-                            />
-                            <textarea
-                                name="message"
-                                placeholder="Message"
-                                required
-                            ></textarea>
+                            <input type="email" name="user_email" placeholder="Email" required />
+                            <input type="text" name="subject" placeholder="Subject" required />
+                            <textarea name="message" placeholder="Message" required />
                             <button type="submit">Send message</button>
                         </form>
                     </div>
                     <div className="location">
-                        <box-icon
-                            name="copyright"
-                            size="20px"
-                            color="grey"
-                        ></box-icon>
-                        <p>
-                            Copyright 2025.{" "}
-                            <span style={{ fontWeight: "bold" }}>
-                                All rights Reserved.
-                            </span>
-                        </p>
+                        <box-icon name="copyright" size="20px" color="grey"></box-icon>
+                        <p>Copyright 2025. <span style={{ fontWeight: "bold" }}>All rights Reserved.</span></p>
                     </div>
                 </div>
             </div>
